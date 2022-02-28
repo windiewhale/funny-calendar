7e45aeb12221ab5c4869d57e8f5132c9591d19b5
windiewhale <feketebodza@gmail.com>
2022-02-28 11:24:44 +0100

    months & days listed

--- frontend/script.js
+++ frontend/script.js
@@ -1,98 +1,92 @@
+const year = [ {
+        name: "January",
+        id: "jan",
+        mth: "01",
+        days: 31,
+    }, {
+        name: "February",
+        id: "feb",
+        mth: "02",
+        days: 28
+    }, {
+        name: "March",
+        id: "mar",
+        mth: "03",
+        days: 31
+    }, {
+        name: "April",
+        id: "apr",
+        mth: "04",
+        days: 30
+    }, {
+        name: "May",
+        id: "may",
+        mth: "05",
+        days: 31
+    }, {
+        name: "June",
+        id: "jun",
+        mth: "06",
+        days: 30
+    }, {
+        name: "July",
+        id: "jul",
+        mth: "07",
+        days: 31
+    }, {
+        name: "August", 
+        id: "aug",
+        mth: "08",
+        days: 31
+    }, {
+        name: "September",
+        id: "sep",
+        mth: "09",
+        days: 30
+    }, {
+        name: "October",
+        id: "oct",
+        mth: "10",
+        days: 31
+    }, {
+        name: "November",
+        id: "nov",
+        mth: "11",
+        days: 30
+    }, {
+        name: "December",
+        id: "dec",
+        mth: "12",
+        days: 31
+    }
+];
+
+
 function loadEvent() {
     let rootElement = document.getElementById("root");
-
-    const year = [ {
-            name: "January",
-            id: "jan",
-            mth: "01",
-            days: 31,
-        }, {
-            name: "February",
-            id: "feb",
-            mth: "02",
-            days: 28
-        }, {
-            name: "March",
-            id: "mar",
-            mth: "03",
-            days: 31
-        }, {
-            name: "April",
-            id: "apr",
-            mth: "04",
-            days: 30
-        }, {
-            name: "May",
-            id: "may",
-            mth: "05",
-            days: 31
-        }, {
-            name: "June",
-            id: "jun",
-            mth: "06",
-            days: 30
-        }, {
-            name: "July",
-            id: "jul",
-            mth: "07",
-            days: 31
-        }, {
-            name: "August", 
-            id: "aug",
-            mth: "08",
-            days: 31
-        }, {
-            name: "September",
-            id: "sep",
-            mth: "09",
-            days: 30
-        }, {
-            name: "October",
-            id: "oct",
-            mth: "10",
-            days: 31
-        }, {
-            name: "November",
-            id: "nov",
-            mth: "11",
-            days: 30
-        }, {
-            name: "December",
-            id: "dec",
-            mth: "12",
-            days: 31
-        }
-    ];
-
-    let month = function (mth, name, day) {
-        return `
-            <div>
-                <time datetime="2022">2022</time>
-                <time datetime="${mth}">${name}</time>
-                <time datetime="${day}">${day}</time>
-            </div>
-        `;
-    }
+    
+    (function listDays() {
         
-    let sectionElements = function(yearArray) {
-        
-        for (const monthList of yearArray) {
-           
-            let monthContainer = `<section id=${monthList.name}>`;
+        for (const monthList of year) {
+
+            let monthContainer = ""
             let dayCount = "";
             
             for (i = 1; i <= monthList.days; i++) {
-                dayCount =+ i
-                monthContainer += month(monthList.mth, monthList.name, dayCount);
+                dayCount += `
+                <div>
+                    <time datetime="YYYY">2022</time>
+                    <time datetime="MM">${monthList.name}</time>
+                    <time datetime="DD">${i}</time>
+                </div>
+                `;
             }
-            monthContainer += `</section>`;
-            return monthContainer
-        }  
-        
-      
-    }
-    rootElement.insertAdjacentHTML("beforeend", sectionElements(year))
-
+    
+            monthContainer += `<section id=${monthList.id} class="month">${dayCount}</section>`;
+            rootElement.insertAdjacentHTML("beforeend", monthContainer)
+        } 
+    }) ();
+    
 };
 
 window.addEventListener("load", loadEvent);
--- frontend/scripty.js
+++ frontend/scripty.js
@@ -105,3 +105,104 @@ function loadEvent() {
 };
 
 window.addEventListener("load", loadEvent);
+
+
+
+//---------------------------another try that lists each days of jan, but only jan--------------------
+
+function loadEvent() {
+    let rootElement = document.getElementById("root");
+
+    const year = [ {
+            name: "January",
+            id: "jan",
+            mth: "01",
+            days: 31,
+        }, {
+            name: "February",
+            id: "feb",
+            mth: "02",
+            days: 28
+        }, {
+            name: "March",
+            id: "mar",
+            mth: "03",
+            days: 31
+        }, {
+            name: "April",
+            id: "apr",
+            mth: "04",
+            days: 30
+        }, {
+            name: "May",
+            id: "may",
+            mth: "05",
+            days: 31
+        }, {
+            name: "June",
+            id: "jun",
+            mth: "06",
+            days: 30
+        }, {
+            name: "July",
+            id: "jul",
+            mth: "07",
+            days: 31
+        }, {
+            name: "August", 
+            id: "aug",
+            mth: "08",
+            days: 31
+        }, {
+            name: "September",
+            id: "sep",
+            mth: "09",
+            days: 30
+        }, {
+            name: "October",
+            id: "oct",
+            mth: "10",
+            days: 31
+        }, {
+            name: "November",
+            id: "nov",
+            mth: "11",
+            days: 30
+        }, {
+            name: "December",
+            id: "dec",
+            mth: "12",
+            days: 31
+        }
+    ];
+
+    let month = function (mth, name, day) {
+        return `
+            <div>
+                <time datetime="2022">2022</time>
+                <time datetime="${mth}">${name}</time>
+                <time datetime="${day}">${day}</time>
+            </div>
+        `;
+    }
+        
+    let sectionElements = function(yearArray) {
+        
+        for (const monthList of yearArray) {
+           
+            let monthContainer = `<section id=${monthList.name}>`;
+            let dayCount = "";
+            
+            for (i = 1; i <= monthList.days; i++) {
+                dayCount =+ i
+                monthContainer += month(monthList.mth, monthList.name, dayCount);
+            }
+            monthContainer += `</section>`;
+            return monthContainer
+        }  
+        
+      
+    }
+    rootElement.insertAdjacentHTML("beforeend", sectionElements(year))
+
+};
